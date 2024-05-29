002acc901e6b4f42375992e37a20f28874204144
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-05-30 00:58:13 +0800

    added initial design

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html, js}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#121212",
+      secondary: "#E0E0E0",
+      tertiary: "#2C2C2C"
+    }
+  },
+  plugins: [],
+}
+
