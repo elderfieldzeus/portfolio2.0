357f97dee849390a7e3ed2ded5b218069a746eed
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-05-31 00:52:11 +0800

    tried animation (2)

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,13 @@ module.exports = {
         primary: "#121212",
         secondary: "#E0E0E0",
         tertiary: "#2C2C2C"
+      },
+      fontFamily: {
+        kiwi_light: ['KiwiMaru-Light', 'sans-serif'],
+        kiwi_medium: ['KiwiMaru-Medium', 'sans-serif'],
+        kiwi_regular: ['KiwiMaru-Regular', 'sans-serif']
       }
-    },
-    fontFamily: {
-      kiwi_light: ['KiwiMaru-Light', 'sans-serif'],
-      kiwi_medium: ['KiwiMaru-Medium', 'sans-serif'],
-      kiwi_regular: ['KiwiMaru-Regular', 'sans-serif']
     }
   },
   plugins: [],
 }
-
