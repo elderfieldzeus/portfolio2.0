e50527c6db5e4f9219aa55919ac7b23998a176b8
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-06-01 03:25:49 +0800

    finally fixed animation woo

--- dist/script/animation.js
+++ dist/script/animation.js
@@ -7,27 +7,37 @@ buttons.forEach((button) => {
 
         const href = button.href;
         // alert(href);
-        nameRise();
+        navbarRise();
     });
 });
 
-// function navAnimation() {
-//     const nav = document.querySelector("nav");
-//     const divs = document.querySelectorAll("nav > div");
-
-//     const rectangles = document.querySelectorAll(".lights");
-//     const [left, right] = rectangles;
-
-//     divs[0].classList.remove("text-5xl");
-//     divs[0].classList.add("text-4xl");
-
-//     nav.classList.add("animate-navRise");
-
-    
-
-//     divs[1].classList.add("hidden");
-// }
-
-function nameRise() {
+function navbarRise() {
+    const navbar = document.getElementById("navbar");
     const name = document.getElementById("name");
+    const softeng = document.getElementById("softeng");
+    const leftLight = document.getElementById("leftLight");
+    const rightLight = document.getElementById("rightLight");
+
+    navbar.style['top'] = 0;
+    navbar.style['left'] = 0;
+    navbar.style['transform'] = 'translate(0,0)';
+    navbar.style['opacity'] = '0';
+    name.style['font-size'] = '2.25rem';
+    name.style['line-height'] = '2.5rem';
+    leftLight.style['left'] = rightLight.style['right']  = '-80rem';
+    leftLight.style['top'] = rightLight.style['top']  = '-60rem';
+    leftLight.style['rotate'] = '25deg';
+    rightLight.style['rotate']  = '-25deg';
+
+    setTimeout(()=> {
+        navbar.style['flex-direction'] = 'row';
+        navbar.style['justify-content'] = 'space-between';
+        navbar.style['z-index'] = 20;
+        softeng.style['display'] = 'none';  
+        navbar.style['opacity'] = '1'; 
+    }, 400);
+
+    setTimeout(()=> {
+        leftLight.style['display'] = rightLight.style['display']  = 'none';     
+    }, 800);
 }
