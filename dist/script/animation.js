e1e160685ab7b51f2ff230f53d659a22977fe9e8
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-06-01 21:25:43 +0800

    added PERFECT animation wahaha

--- dist/script/animation.js
+++ dist/script/animation.js
@@ -1,22 +1,37 @@
 const buttons = document.querySelectorAll("li > a");
-const nav = document.querySelector("nav");
+const navbar = document.getElementById("navbar");
+const name = document.getElementById("name");
 
 buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
-        e.preventDefault();
+        if(!navbar.classList.contains('home')) {
+            e.preventDefault();
 
-        const href = button.href;
-        // alert(href);
-        navbarRise();
+            const href = button.href;
+            navbarRise(href);
+            navbar.classList.add('home');
+        }
     });
 });
 
-function navbarRise() {
-    const navbar = document.getElementById("navbar");
-    const name = document.getElementById("name");
+name.addEventListener("click", () => {
+    if(navbar.classList.contains('home')) {
+        window.location.href = '#home';
+        setTimeout(() => {
+            navbarFade();
+            navbar.classList.remove('home');
+        }, 800);
+        
+    }
+});
+
+function navbarRise(href) {
     const softeng = document.getElementById("softeng");
     const leftLight = document.getElementById("leftLight");
     const rightLight = document.getElementById("rightLight");
+
+    const sections = document.querySelectorAll("section");
+    const footer = document.querySelector("footer");
 
     navbar.style['top'] = 0;
     navbar.style['left'] = 0;
@@ -29,15 +44,76 @@ function navbarRise() {
     leftLight.style['rotate'] = '25deg';
     rightLight.style['rotate']  = '-25deg';
 
+    sections.forEach((section) => {
+        section.style['display'] = 'flex';
+    });
+
+    footer.style['display'] = 'block';
+
     setTimeout(()=> {
         navbar.style['flex-direction'] = 'row';
         navbar.style['justify-content'] = 'space-between';
         navbar.style['z-index'] = 20;
         softeng.style['display'] = 'none';  
-        navbar.style['opacity'] = '1'; 
+        navbar.style['opacity'] = '1';
+
+        sections.forEach((section) => {
+            section.style['opacity'] = '1';
+        });
+
+        footer.style['opacity'] = '1';
     }, 400);
 
     setTimeout(()=> {
-        leftLight.style['display'] = rightLight.style['display']  = 'none';     
+        leftLight.style['display'] = rightLight.style['display']  = 'none';
+        window.location.href = href;
+    }, 1000);
+}
+
+function navbarFade() {
+    const navbar = document.getElementById("navbar");
+    const softeng = document.getElementById("softeng");
+    const leftLight = document.getElementById("leftLight");
+    const rightLight = document.getElementById("rightLight");
+
+    const sections = document.querySelectorAll("section");
+    const footer = document.querySelector("footer");
+
+    leftLight.style['display'] = rightLight.style['display']  = 'block';
+
+    navbar.style['z-index'] = 0;
+    navbar.style['top'] = '50%';
+    navbar.style['left'] = '50%';
+    navbar.style['transform'] = 'translate(-50%,-50%)';
+    navbar.style['opacity'] = '0';
+    name.style['font-size'] = '3rem';
+    name.style['line-height'] = '1';
+
+    sections.forEach((section) => {
+        section.style['opacity'] = '0';
+    });
+
+    footer.style['opacity'] = '0';
+
+    setTimeout(() => {
+        leftLight.style['left'] = rightLight.style['right']  = '-18rem';
+        leftLight.style['top'] = rightLight.style['top']  = '-9rem';
+        leftLight.style['rotate'] = '12deg';
+        rightLight.style['rotate']  = '-12deg';
+    }, 400);
+
+    setTimeout(()=> {
+        navbar.style['flex-direction'] = 'column';
+        navbar.style['justify-content'] = 'center';
+        softeng.style['display'] = 'block';  
+        navbar.style['opacity'] = '1';
+
+        footer.style['display'] = 'none';
     }, 800);
+
+    setTimeout(() => {
+        sections.forEach((section) => {
+            section.style['display'] = 'none';
+        });
+    }, 500)
 }
